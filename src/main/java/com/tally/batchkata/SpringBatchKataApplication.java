572b8717212d81db73390abc8203cb6351348016
ahn-sj <jaesa5221@gmail.com>
2024-09-21 21:10:24 +0900

    init: project init

---
+++ src/main/java/com/tally/batchkata/SpringBatchKataApplication.java
@@ -0,0 +1,13 @@
+package com.tally.batchkata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatchKataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBatchKataApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tally/batchkata/SpringBatchKataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tally.batchkata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchKataApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
